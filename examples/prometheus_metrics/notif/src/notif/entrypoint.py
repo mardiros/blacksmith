e28d9e003f6daf5facd8e8a31ba5964574d0fece
Guillaume Gauvrit <guillaume@gauvr.it>
2021-11-28 15:47:17 +0100

    Add example runing prometheus

---
+++ examples/prometheus_metrics/notif/src/notif/entrypoint.py
@@ -0,0 +1,8 @@
+from typing import cast
+
+import uvicorn
+from asgiref.typing import ASGI3Application
+from notif.views import app
+
+if __name__ == "__main__":
+    uvicorn.run(cast(ASGI3Application, app), host="0.0.0.0", port=8000)
