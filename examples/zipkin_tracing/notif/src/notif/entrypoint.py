f6519f322472f3ee3fab762a9de7607afc7d013e
Guillaume Gauvrit <guillaume@gauvr.it>
2021-12-16 20:59:08 +0100

    Add exemple of zipkin middleware

--- examples/zipkin_tracing/notif/src/notif/entrypoint.py
+++ examples/zipkin_tracing/notif/src/notif/entrypoint.py
@@ -1,15 +1,24 @@
+import json
 from typing import cast
 
 import uvicorn
 from asgiref.typing import ASGI3Application
-from notif.blacksmith_middleware import BlacksmithMiddleware
 from notif.views import app
-from notif.zk_middleware import ZipkinMiddleware
+from starlette_zipkin import B3Headers, ZipkinConfig, ZipkinMiddleware
 
 import blacksmith
 
 if __name__ == "__main__":
     blacksmith.scan("notif.resources")
-    app.add_middleware(BlacksmithMiddleware)
-    app.add_middleware(ZipkinMiddleware, service_name="notif-v1")
+    config = ZipkinConfig(
+        host="zipkin",
+        port=9411,
+        service_name="notif-v1",
+        sample_rate=1.0,
+        inject_response_headers=True,
+        force_new_trace=False,
+        json_encoder=json.dumps,
+        header_formatter=B3Headers,
+    )
+    app.add_middleware(ZipkinMiddleware, config=config)
     uvicorn.run(cast(ASGI3Application, app), host="0.0.0.0", port=8000)
