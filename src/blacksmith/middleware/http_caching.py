7c5c6908fa94e0a7c5dd938fadf4d174df840387
Guillaume Gauvrit <guillaume@gauvr.it>
2021-12-21 18:50:46 +0100

    Add a policy class to define the caching policy

--- src/blacksmith/middleware/http_caching.py
+++ src/blacksmith/middleware/http_caching.py
@@ -3,7 +3,7 @@ import abc
 import json
 from dataclasses import asdict
 from datetime import timedelta
-from typing import List, Optional, TYPE_CHECKING, Any, Callable
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Tuple
 from urllib.parse import urlencode
 
 from blacksmith.domain.model.http import HTTPRequest, HTTPResponse
@@ -11,6 +11,45 @@ from blacksmith.domain.model.params import Request
 from blacksmith.typing import ClientName, HttpMethod, Path
 
 from .base import HTTPMiddleware, Middleware
+
+
+class AbstractCachingPolicy(abc.ABC):
+    """Caching Policy"""
+
+    @abc.abstractmethod
+    def handle_request(
+        self, req: HTTPRequest, method: HttpMethod, client_name: ClientName, path: Path
+    ) -> bool:
+        """A function to decide if the http request is cachable."""
+
+    @abc.abstractmethod
+    def get_vary_key(
+        self,
+        client_name: ClientName,
+        path: Path,
+        request: HTTPRequest,
+    ) -> str:
+        """Create a caching key for the vary part."""
+
+    @abc.abstractmethod
+    def get_response_cache_key(
+        self,
+        client_name: ClientName,
+        path: Path,
+        req: HTTPRequest,
+        vary: List[str],
+    ) -> str:
+        """Create a caching key for the http response."""
+
+    @abc.abstractmethod
+    def get_cache_info_for_response(
+        self,
+        client_name: ClientName,
+        path: Path,
+        req: HTTPRequest,
+        resp: HTTPResponse,
+    ) -> Tuple[int, str, List[str]]:
+        """Return caching info. Tuple (ttl in seconds, vary key, vary list)."""
 
 
 class AbstractCache(abc.ABC):
@@ -31,6 +70,7 @@ class AbstractCache(abc.ABC):
 
 try:
     from aioredis import Redis
+
     AbstractCache.register(Redis)
 except ImportError:
     pass
@@ -57,18 +97,63 @@ def get_max_age(response: HTTPResponse) -> int:
     return max(max_age - age, 0)
 
 
-def get_vary_key(path: Path, request: HTTPRequest) -> str:
-    path = path.format(**request.path)
-    if request.querystring:
-        qs = urlencode(request.querystring, doseq=True)
-        path = f"{path}?{qs}"
-    return path
-
-
 def get_vary_header_split(response: HTTPResponse) -> List[str]:
     vary = response.headers.get("vary", "")
     fields = [field.strip().lower() for field in vary.split(",")] if vary else []
     return fields
+
+
+class CacheControlPolicy(AbstractCachingPolicy):
+    """
+    Initialize the caching using `Cache-Control` http headers.
+    Also consume the `Vary` response header to cache response per
+    Vary response headers per request.
+
+    :param sep: Separator used in cache key **MUST NOT BE USED** in client name.
+    """
+
+    def __init__(self, sep: str) -> None:
+        self.sep = sep
+
+    def handle_request(
+        self, req: HTTPRequest, method: HttpMethod, client_name: ClientName, path: Path
+    ) -> bool:
+        return method == "GET"
+
+    def get_vary_key(
+        self, client_name: ClientName, path: Path, request: HTTPRequest
+    ) -> str:
+        path = path.format(**request.path)
+        if request.querystring:
+            qs = urlencode(request.querystring, doseq=True)
+            path = f"{path}?{qs}"
+        return f"{client_name}{self.sep}{path}"
+
+    def get_response_cache_key(
+        self,
+        client_name: ClientName,
+        path: Path,
+        req: HTTPRequest,
+        vary: List[str],
+    ) -> str:
+        vary_key = self.get_vary_key(client_name, path, req)
+        vary_dict = {key: req.headers.get(key, "") for key in vary}
+        response_cache_key = f"{vary_key}${json.dumps(vary_dict)}"
+        return response_cache_key
+
+    def get_cache_info_for_response(
+        self,
+        client_name: ClientName,
+        path: Path,
+        req: HTTPRequest,
+        resp: HTTPResponse,
+    ) -> Tuple[int, str, List[str]]:
+        max_age = get_max_age(resp)
+        if max_age <= 0:
+            return (max_age, "", [])
+        vary_key = self.get_vary_key(client_name, path, req)
+        vary = get_vary_header_split(resp)
+        return (max_age, vary_key, vary)
 
 
 class HttpCachingMiddleware(HTTPMiddleware):
@@ -76,63 +161,72 @@ class HttpCachingMiddleware(HTTPMiddleware):
     Zipkin Middleware based on aiozipkin
     """
 
-    def __init__(self, cache: AbstractCache) -> None:
+    def __init__(
+        self,
+        cache: AbstractCache,
+        policy: AbstractCachingPolicy = CacheControlPolicy(sep="$"),
+    ) -> None:
         self._cache = cache
+        self._policy = policy
 
-    def handle_request(
-        self, req: HTTPRequest, method: HttpMethod, client_name: ClientName, path: Path
-    ) -> bool:
-        return method == "GET"
+    async def initialize(self):
+        await self._cache.initialize()
 
     async def cache_response(
         self,
+        client_name: ClientName,
         path: Path,
         req: HTTPRequest,
         resp: HTTPResponse,
     ):
-        max_age = get_max_age(resp)
-        if max_age <= 0:
+        (
+            ttl,
+            vary_key,
+            vary,
+        ) = self._policy.get_cache_info_for_response(client_name, path, req, resp)
+        if ttl <= 0:
             return
-        ttl = timedelta(seconds=max_age)
-        vary_key = get_vary_key(path, req)
-        vary = get_vary_header_split(resp)
+        ttld = timedelta(seconds=ttl)
         vary_val = json.dumps(vary)
-        await self._cache.set(vary_key, vary_val, ttl)
-        vary_dict = {key: req.headers.get(key, "") for key in vary}
-        resonse_cache_key = f"{vary_key}${json.dumps(vary_dict)}"
+        await self._cache.set(vary_key, vary_val, ttld)
+
+        response_cache_key = self._policy.get_response_cache_key(
+            client_name, path, req, vary
+        )
+        resp.headers = dict(resp.headers)
         response_cache = json.dumps(asdict(resp))
-        await self._cache.set(resonse_cache_key, response_cache, ttl)
+        await self._cache.set(response_cache_key, response_cache, ttld)
 
     async def get_from_cache(
-        self, path: Path, req: HTTPRequest
+        self, client_name: ClientName, path: Path, req: HTTPRequest
     ) -> Optional[HTTPResponse]:
-        vary_key = get_vary_key(path, req)
+        vary_key = self._policy.get_vary_key(client_name, path, req)
         vary_val = await self._cache.get(vary_key)
         if not vary_val:
             return None
         vary = json.loads(vary_val)
-        vary_dict = {key: req.headers.get(key, "") for key in vary}
-        resonse_cache_key = f"{vary_key}${json.dumps(vary_dict)}"
-        val = await self._cache.get(resonse_cache_key)
+        response_cache_key = self._policy.get_response_cache_key(
+            client_name, path, req, vary
+        )
+        val = await self._cache.get(response_cache_key)
         if not val:
             return None
         resp = json.loads(val)
         return HTTPResponse(**resp)
-
 
     def __call__(self, next: Middleware) -> Middleware:
         async def handle(
             req: HTTPRequest, method: HttpMethod, client_name: ClientName, path: Path
         ) -> HTTPResponse:
 
-            if not self.handle_request(req, method, client_name, path):
+            if not self._policy.handle_request(req, method, client_name, path):
                 return await next(req, method, client_name, path)
 
-            resp = await self.get_from_cache(path, req)
+            resp = await self.get_from_cache(client_name, path, req)
             if resp:
                 return resp
             resp = await next(req, method, client_name, path)
-            await self.cache_response(path, req, resp)
+            await self.cache_response(client_name, path, req, resp)
             return resp
 
         return handle
--- tests/unittests/test_middleware_http_caching.py
+++ tests/unittests/test_middleware_http_caching.py
@@ -1,14 +1,15 @@
 import json
 
 import pytest
-from blacksmith.middleware.http_caching import (
-    HttpCachingMiddleware,
-    get_vary_header_split,
-    get_vary_key,
-    int_or_0,
-    get_max_age,
-)
+
 from blacksmith.domain.model.http import HTTPRequest, HTTPResponse
+from blacksmith.middleware.http_caching import (
+    CacheControlPolicy,
+    HttpCachingMiddleware,
+    get_max_age,
+    get_vary_header_split,
+    int_or_0,
+)
 
 
 @pytest.mark.parametrize("params", [("0", 0), ("42", 42), ("2.5", 0), ("xxx", 0)])
@@ -45,26 +46,6 @@ def test_get_max_age(params):
 @pytest.mark.parametrize(
     "params",
     [
-        ("/", HTTPRequest("", {}, {}), "/"),
-        ("/names/{name}", HTTPRequest("", {"name": "dummy"}, {}), "/names/dummy"),
-        ("/names", HTTPRequest("", {}, {"name": "dummy"}), "/names?name=dummy"),
-        (
-            "/names/{name}",
-            HTTPRequest("", {"name": "dummy"}, {"foo": "bar"}),
-            "/names/dummy?foo=bar",
-        ),
-        ("/", HTTPRequest("", {}, {"foo": ["egg", "bar"]}), "/?foo=egg&foo=bar"),
-        ("/", HTTPRequest("", {}, {"foo": ["e g", "bàr"]}), "/?foo=e+g&foo=b%C3%A0r"),
-        ("/", HTTPRequest("", {}, {"foo": ["our$sep"]}), "/?foo=our%24sep"),
-    ],
-)
-def test_get_vary_key(params):
-    assert get_vary_key(params[0], params[1]) == params[2]
-
-
-@pytest.mark.parametrize(
-    "params",
-    [
         (HTTPResponse(200, {}, ""), []),
         (HTTPResponse(200, {"vary": "Encoding"}, ""), ["encoding"]),
         (
@@ -75,6 +56,139 @@ def test_get_vary_key(params):
 )
 def test_get_vary_header_split(params):
     assert get_vary_header_split(params[0]) == params[1]
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        ("GET", True),
+        ("HEAD", False),
+        ("POST", False),
+        ("PUT", False),
+        ("PATCH", False),
+        ("DELETE", False),
+        ("OPTIONS", False),
+    ],
+)
+def test_policy_handle_request(params):
+    policy = CacheControlPolicy("$")
+    req = HTTPRequest("/")
+    method, expected = params
+    assert policy.handle_request(req, method, "", "") == expected
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        ("dummies", "/", HTTPRequest("", {}, {}), "dummies$/"),
+        ("bar", "/", HTTPRequest("", {}, {}), "bar$/"),
+        (
+            "dummies",
+            "/names/{name}",
+            HTTPRequest("", {"name": "dummy"}, {}),
+            "dummies$/names/dummy",
+        ),
+        (
+            "dummies",
+            "/names",
+            HTTPRequest("", {}, {"name": "dummy"}),
+            "dummies$/names?name=dummy",
+        ),
+        (
+            "dummies",
+            "/names/{name}",
+            HTTPRequest("", {"name": "dummy"}, {"foo": "bar"}),
+            "dummies$/names/dummy?foo=bar",
+        ),
+        (
+            "dummies",
+            "/",
+            HTTPRequest("", {}, {"foo": ["egg", "bar"]}),
+            "dummies$/?foo=egg&foo=bar",
+        ),
+        (
+            "dummies",
+            "/",
+            HTTPRequest("", {}, {"foo": ["e g", "bàr"]}),
+            "dummies$/?foo=e+g&foo=b%C3%A0r",
+        ),
+        (
+            "dummies",
+            "/",
+            HTTPRequest("", {}, {"foo": ["our$sep"]}),
+            "dummies$/?foo=our%24sep",
+        ),
+    ],
+)
+def test_policy_get_vary_key(params):
+    policy = CacheControlPolicy("$")
+    assert policy.get_vary_key(params[0], params[1], params[2]) == params[3]
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        (
+            "dummies",
+            "/",
+            HTTPRequest("", headers={"Accept-Encoding": "gzip"}),
+            [],
+            "dummies$/${}",
+        ),
+        (
+            "dummies",
+            "/",
+            HTTPRequest("", headers={"Accept-Encoding": "gzip"}),
+            ["Accept-Encoding"],
+            'dummies$/${"Accept-Encoding": "gzip"}',
+        ),
+        (
+            "dummies",
+            "/",
+            HTTPRequest("", headers={}),
+            ["Accept-Encoding"],
+            'dummies$/${"Accept-Encoding": ""}',
+        ),
+    ],
+)
+def test_policy_get_response_cache_key(params):
+    policy = CacheControlPolicy("$")
+    assert (
+        policy.get_response_cache_key(params[0], params[1], params[2], params[3])
+        == f"{params[4]}"
+    )
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        ("dummies", "/", HTTPRequest(""), HTTPResponse(200, {}, ""), (0, "", [])),
+        (
+            "dummies",
+            "/",
+            HTTPRequest("", headers={"Cache-Control": "max-age=60, public"}),
+            HTTPResponse(200, {"cache-control": "max-age=60, public"}, ""),
+            (60, "dummies$/", []),
+        ),
+        (
+            "dummies",
+            "/",
+            HTTPRequest("", headers={"Accept-Encoding": "gzip"}),
+            HTTPResponse(
+                200,
+                {"vary": "Accept-Encoding", "cache-control": "max-age=60, public"},
+                "",
+            ),
+            (60, "dummies$/", ["accept-encoding"]),
+        ),
+    ],
+)
+def test_policy_get_cache_info_for_response(params):
+    policy = CacheControlPolicy("$")
+    assert (
+        policy.get_cache_info_for_response(params[0], params[1], params[2], params[3])
+        == params[4]
+    )
 
 
 @pytest.mark.parametrize(
@@ -91,8 +205,8 @@ def test_get_vary_header_split(params):
             HTTPRequest("", {}, {}),
             HTTPResponse(200, {"cache-control": "max-age=42, public"}, ""),
             {
-                "/": (42, "[]"),
-                "/${}": (
+                "dummies$/": (42, "[]"),
+                "dummies$/${}": (
                     42,
                     '{"status_code": 200, "headers": {"cache-control": "max-age=42, '
                     'public"}, "json": ""}',
@@ -108,8 +222,8 @@ def test_get_vary_header_split(params):
                 "En Francais",
             ),
             {
-                "/": (42, '["x-country-code"]'),
-                '/${"x-country-code": "FR"}': (
+                "dummies$/": (42, '["x-country-code"]'),
+                'dummies$/${"x-country-code": "FR"}': (
                     42,
                     '{"status_code": 200, "headers": '
                     '{"cache-control": "max-age=42, public", "vary": '
@@ -126,8 +240,8 @@ def test_get_vary_header_split(params):
                 "missing_header",
             ),
             {
-                "/": (42, '["x-country-code"]'),
-                '/${"x-country-code": ""}': (
+                "dummies$/": (42, '["x-country-code"]'),
+                'dummies$/${"x-country-code": ""}': (
                     42,
                     '{"status_code": 200, "headers": '
                     '{"cache-control": "max-age=42, public", "vary": '
@@ -144,8 +258,8 @@ def test_get_vary_header_split(params):
                 "many_headers",
             ),
             {
-                "/": (42, '["a", "b"]'),
-                '/${"a": "A", "b": "B"}': (
+                "dummies$/": (42, '["a", "b"]'),
+                'dummies$/${"a": "A", "b": "B"}': (
                     42,
                     '{"status_code": 200, "headers": '
                     '{"cache-control": "max-age=42, public", "vary": '
@@ -156,46 +270,33 @@ def test_get_vary_header_split(params):
     ],
 )
 @pytest.mark.asyncio
-async def test_http_caching(params, fake_http_middleware_cache):
+async def test_http_cache_response(params, fake_http_middleware_cache):
     middleware = HttpCachingMiddleware(fake_http_middleware_cache)
     path, req, resp, expected = params
-    await middleware.cache_response(path, req, resp)
+    await middleware.cache_response("dummies", path, req, resp)
     assert fake_http_middleware_cache.val == expected
 
-    resp_from_cache = await middleware.get_from_cache(path, req)
+    resp_from_cache = await middleware.get_from_cache("dummies", path, req)
     if expected:
         assert resp_from_cache == resp
     else:
         assert resp_from_cache is None
 
 
-@pytest.mark.parametrize(
-    "params",
-    [
-        ("GET", True),
-        ("HEAD", False),
-        ("POST", False),
-        ("PUT", False),
-        ("PATCH", False),
-        ("DELETE", False),
-        ("OPTIONS", False),
-    ],
-)
-def test_handle_request(params, fake_http_middleware_cache):
-    middleware = HttpCachingMiddleware(fake_http_middleware_cache)
-    req = HTTPRequest("/")
-    method, expected = params
-    assert middleware.handle_request(req, method, "", "") == expected
-
 @pytest.mark.asyncio
-async def test_cache_middleware(cachable_response, boom_middleware, fake_http_middleware_cache, dummy_http_request):
+async def test_cache_middleware(
+    cachable_response, boom_middleware, fake_http_middleware_cache, dummy_http_request
+):
     caching = HttpCachingMiddleware(fake_http_middleware_cache)
     next = caching(cachable_response)
     resp = await next(dummy_http_request, "GET", "dummy", "/dummies/{name}")
-    assert resp == HTTPResponse(200, {"cache-control": "max-age=42, public"}, json="Cache Me")
+    assert resp == HTTPResponse(
+        200, {"cache-control": "max-age=42, public"}, json="Cache Me"
+    )
 
     # get from the cache, not from the boom which raises
     next = caching(boom_middleware)
     resp = await next(dummy_http_request, "GET", "dummy", "/dummies/{name}")
-    assert resp == HTTPResponse(200, {"cache-control": "max-age=42, public"}, json="Cache Me")
-
+    assert resp == HTTPResponse(
+        200, {"cache-control": "max-age=42, public"}, json="Cache Me"
+    )
