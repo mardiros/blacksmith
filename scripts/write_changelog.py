286c85a0d8bf0bf415a1f2a9110d1930e460c05b
Guillaume Gauvrit <guillaume@gauvr.it>
2022-01-19 13:44:59 +0100

    Fix flake8

--- scripts/write_changelog.py
+++ scripts/write_changelog.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 import datetime
 import blacksmith
-header = f"{blacksmith.__version__}  - Released on {datetime.datetime.now().date().isoformat()}"
-with open("CHANGELOG.rst.new", 'w') as changelog:
+
+header = (
+    f"{blacksmith.__version__}  - "
+    f"Released on {datetime.datetime.now().date().isoformat()}"
+)
+with open("CHANGELOG.rst.new", "w") as changelog:
     changelog.write(header)
     changelog.write("\n")
     changelog.write("-" * len(header))
