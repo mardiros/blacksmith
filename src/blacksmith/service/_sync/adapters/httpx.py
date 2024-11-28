af7a9f16f67fd2881791b1387778e26bff3e32a2
Guillaume Gauvrit <guillaume@gauvr.it>
2024-11-28 17:43:10 +0100

    Update httpx

--- src/blacksmith/service/_sync/adapters/httpx.py
+++ src/blacksmith/service/_sync/adapters/httpx.py
@@ -43,7 +43,7 @@ class SyncHttpxTransport(SyncAbstractTransport):
         headers = build_headers(req)
         with SyncClient(
             verify=self.verify_certificate,
-            proxies=self.proxies,  # type: ignore
+            mounts=self.proxies,  # type: ignore
         ) as client:
             try:
                 r = client.request(  # type: ignore
